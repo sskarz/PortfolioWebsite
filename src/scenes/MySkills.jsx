a2456a3f4cb499f0d7016fcb99b0e30f75939783
sskarz <sanskar1016pro@outlook.com>
2024-10-29 02:31:17 -0700

    Initial commit

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,162 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* Header and Image Section */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            My{" "}
+            <span className="text-red hover:text-blue transition duration-500">
+              Skills
+            </span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+            My skillset is diversified in all aspects of software engineering!
+            From building full-stack web apps to fine-tuning AI models, and even
+            configuring time-series databases!
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0">
+          {isAboveMediumScreens ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+            before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="assets/skills-image.png"
+              />
+            </div>
+          ) : (
+            <img alt="skills" className="z-10" src="assets/skills-image.png" />
+          )}
+        </div>
+      </div>
+      {/* Skills Section */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* Experience */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Programming Languages
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]"></div>
+          </div>
+          <div className="mt-5">
+            <ul>
+              <li>Python</li>
+              <li>JavaScript/TypeScript</li>
+              <li>C#</li>
+              <li>Java</li>
+              <li>SQL</li>
+              <li>C++</li>
+              <li>C</li>
+            </ul>
+          </div>
+        </motion.div>
+
+        {/* Innovative */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">02</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Tools and Technologies
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]"></div>
+          </div>
+          <div className="mt-5">
+            <ul>
+              <li>Github</li>
+              <li>Docker</li>
+              <li>JIRA</li>
+              <li>Microsoft Azure</li>
+              <li>Amazon Web Services</li>
+              <li>Linux</li>
+              <li>Agile Development</li>
+            </ul>
+          </div>
+        </motion.div>
+
+        {/* Imaginative */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">03</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Frameworks and Libraries
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]"></div>
+          </div>
+          <div className="mt-5">
+            <ul>
+              <li>React</li>
+              <li>PyTorch</li>
+              <li>NodeJS</li>
+              <li>Tailwind CSS</li>
+              <li>TensorFlow</li>
+              <li>MongoDB</li>
+              <li>Django</li>
+            </ul>
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
---
+++ src/scenes/Projects.jsx
@@ -0,0 +1,110 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const Project = ({ title, subtitle, project_title, link }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{project_title}</p>
+        <p className="mt-7">{subtitle}</p>
+        <a href={link} className="mt-7" target="_blank" rel="noreferrer">
+          <u>Link</u>
+        </a>
+      </div>
+      <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />
+    </motion.div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      {/* HEADINGS */}
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red hover:text-blue transition duration-500">
+              Pro
+            </span>
+            jects
+          </p>
+          <div className="flex justify-center mt-5">
+            <LineGradient width="w-2/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+          Always focused on exploring unexplored avenues in software!<br></br>{" "}
+          Full-stack development and AI are my forte!
+        </p>
+      </motion.div>
+
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+        >
+          {/* ROW 1 */}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-red hover:bg-blue transition duration-500
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            Fullstack Development & Artificial Intelligence
+          </div>
+          <Project
+            title="Project-1"
+            subtitle="A website where users can find books, post books, write reviews, and engage in discussions on a message board!"
+            project_title="Book Exchange Website"
+            link="https://github.com/sskarz/Book-Store-Web-Project"
+          />
+          <Project
+            title="Project-2"
+            subtitle="A custom fine-tuned GPT-2 model that imitates user input to generate text!"
+            project_title="GPT-2 AI User Imitation"
+            link="https://github.com/sskarz/GPT-2-AI-User-Imitation"
+          />
+
+          {/* <div
+            className="flex justify-center text-center items-center p-10 bg-blue
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            SMOOTH USER EXPERIENCE
+          </div> */}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
